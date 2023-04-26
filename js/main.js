a9ecc6c4404b55aaf8f40fccea4d13e1ff99f98e
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-04-26 19:50:19 +0200

    adw

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@
   function vibrate() {
     window.navigator.vibrate([200, 100, 200]);
   }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      // wyświetlenie lokalizacji na mapie
+      // ...
+    });
+  } else {
+    alert("Twoja przeglądarka nie obsługuje geolokalizacji.");
+  }
+  
