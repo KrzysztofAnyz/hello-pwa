76905b86e0bd5b40c5cb2d066d453ac06582a4f0
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-04-26 20:13:30 +0200

    fawf

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@
   function vibrate() {
     window.navigator.vibrate([200]);
   }
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    document.getElementById("coords").innerHTML = `Szerokość: ${latitude}, Długość: ${longitude}`;
+  }
+  function showError(error) {
+    document.getElementById("coords").innerHTML = "Nie udało się pobrać Twojej lokalizacji.";
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    document.getElementById("coords").innerHTML = "Twoja przeglądarka nie obsługuje Geolocation.";
+  }
