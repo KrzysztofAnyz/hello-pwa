3ec7aa4d6612b59aad92c63ca78e6f6826dd0289
KrzysztofAnyz <s220034@student.uek.krakow.pl>
2023-04-26 20:32:45 +0200

    test

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@
 
   navigator.geolocation.getCurrentPosition(function(position) {
     var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
       maxZoom: 18
     }).addTo(map);
